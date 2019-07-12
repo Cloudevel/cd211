d26bff754186937b5ef5471a3ad536764841c7ad
Jose Luis Chiquete <josech@gmail.com>
2019-07-11 18:39:34 -0700

    180711c

---
+++ ejemplos/02/circulo.js
@@ -0,0 +1,5 @@
+function cambia_colores(){
+    let estilo = document.getElementById("circulo").style;
+        if (estilo.backgroundColor == "gold") {estilo.backgroundColor = "skyblue"}
+        else {estilo.backgroundColor = "gold"}
+            }
--- ejemplos/02/saluda.js
+++
@@ -1,5 +0,0 @@
-function despliega(){
-    let entrada_de_texto = document.getElementById("nombre");
-    let despliega_mensaje = document.getElementById("rotulo");
-    despliega_mensaje.textContent = "Hola, " + nombre.value + ".";
-}
